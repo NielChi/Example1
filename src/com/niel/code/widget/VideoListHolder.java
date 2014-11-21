2e90f7ec631aa02cbfb1efbcdd0b897634c42713
NielChi <nielchi0714@gmail.com>
2014-11-21 11:23:22 +0800

    add function, can get read recorded video.

---
+++ src/com/niel/code/widget/VideoListHolder.java
@@ -0,0 +1,13 @@
+package com.niel.code.widget;
+
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class VideoListHolder{
+	ImageView mScreenshot;
+	TextView mVideoName;
+	TextView mVideoFileName;
+	CheckBox isDeleteCheck;
+}
---
+++ src/com/niel/code/widget/VideoListStructure.java
@@ -0,0 +1,62 @@
+package com.niel.code.widget;
+
+import android.graphics.Bitmap;
+
+public class VideoListStructure {
+	private Bitmap mBitmap;
+	private String mVideoName;
+	private String mVideoFileName;
+	private String mVideoPath;
+	private boolean isDelete;
+	
+	public VideoListStructure() {
+		
+	}
+	
+	public void setScreenshot(Bitmap bitmap) {
+		this.mBitmap = bitmap;
+	}
+	
+	public Bitmap getScreenshot() {
+		return this.mBitmap;
+	}
+	
+	public void setVideoName(String name) {
+		this.mVideoName = name;
+	}
+	
+	public String getVideoName() {
+		return this.mVideoName;
+	}
+	
+	public void setVideoFileName(String name) {
+		this.mVideoFileName = name;
+	}
+	
+	public String getVideoFileName() {
+		return this.mVideoFileName;
+	}
+	
+	public void setVideoPath(String path) {
+		this.mVideoPath = path;
+	}
+	
+	public String getVideoPath() {
+		return this.mVideoPath;
+	}
+	
+	public void deleteFile(boolean delete) {
+		isDelete = delete;
+	}
+	
+	public boolean isDetele() {
+		return isDelete;
+	}
+	
+	public void releaseMemory() {
+		this.mBitmap.recycle();
+		this.mBitmap = null;
+		this.mVideoName = null;
+		this.mVideoFileName = null;
+	}
+}
